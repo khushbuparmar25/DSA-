75e7d6e81795a5dbe31a71c3429f7400d399d7cf
khushbuparmar25 <66319265+khushbuparmar25@users.noreply.github.com>
2021-10-02 18:10:18 +0530

    DAY2

---
+++ DSA Codes/binarySearch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int binarySearch(int arr[], int size, int element)
+{
+    int low, high, mid;
+    low = 0;
+    high = size - 1;
+    while(low <= high)
+    {
+        mid = (low + high)/2;
+        if(arr[mid] == element)
+        {
+            printf("Element %d found at index %d \n", element, mid);
+            return mid;
+        }
+        if(element > arr[mid] )
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    printf("Element not found \n");
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1,3,5,10,23,37,45,69,80,1000}, element = 1000;
+    int size = sizeof(arr)/sizeof(int);
+    binarySearch(arr, size, element);
+    return 0;
+}
---
+++ DSA Codes/linearSearch.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int linearSearch(int arr[], int size, int element)
+{
+    for(int i = 0; i < size; i++)
+    {
+        if(element==arr[i])
+        {
+            printf("Element %d found at index %d \n",element, i);
+            return i;
+        }
+    }
+    printf("Element not found \n");
+    return -1;
+}
+
+int main()
+{
+    int arr[50] = {2,65,32,6,15,100}, element = 100;
+    int size = sizeof(arr)/ sizeof(int);
+    linearSearch(arr, size, element);
+    return 0;
+}
